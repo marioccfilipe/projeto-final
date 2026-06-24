93cf40e1779aceacc095bae10ceca70387343623
marioccfilipe <marioccfilipe@gmail.com>
2026-06-24 19:11:48 -0300

    alugueis funcionando

--- booknet/src/main.jsx
+++ booknet/src/main.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+
 import { BookProvider } from "./context/BookContext";
 import { ClientProvider } from "./context/ClientContext";
 import { RentalProvider } from "./context/RentalContext";
